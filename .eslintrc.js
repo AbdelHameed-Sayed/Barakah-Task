417b6a20cda8e271858cc8fbffc7fe1d5dae184e
AbdelHameed Sayed <abali@aaib.com>
2024-08-22 11:45:59 +0300

    feat: expo intialization with eslint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  root: true,
+  extends: ['expo', 'eslint:recommended'],
+  plugins: ['import', 'react-native', 'prettier'],
+  rules: {
+    'no-console': 'error',
+    'no-empty': ['error', {allowEmptyCatch: true}],
+    'import/no-unresolved': 'error',
+    'import/export': 'error',
+    'import/no-deprecated': 'error',
+    'import/named': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native|expo)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@(atoms|molecules|organisms|screens)/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-return-await': 'error',
+    'no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+      },
+    ],
+    'no-duplicate-imports': 'error',
+    'array-callback-return': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/ignore': ['node_modules'],
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  ignorePatterns: [
+    '*.test.js',
+    '*.test.ts',
+    '*.test.tsx',
+    '**/__tests__/**',
+    '**/tests/**',
+  ],
+};
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {useEffect} from 'react';
+
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {useColorScheme} from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -29,7 +30,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
